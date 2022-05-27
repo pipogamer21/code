258aa5687409c0686a339f2e671b7200c5d1047d
pipogamer21 <45886370+pipogamer21@users.noreply.github.com>
2022-05-27 16:39:06 +0200

    Update and rename guardado.php to index.php

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+    <head>
+        <title>php</title>
+    </head>
+    <body>
+        <form method="POST" action="index.php" id="request" class="main_form" autocomplete="on">
+                     <div class="row">
+                        <div class="col-md-12 ">
+                           <input class="contactus" placeholder="Nombre Completo" type="text" name="full_name" required> 
+                        </div>
+                        <div class="col-md-12">
+                           <input class="contactus" placeholder="Teléfono" type="tel" name="tel" required> 
+                        </div>
+                        <div class="col-md-12">
+                           <input class="contactus" placeholder="Email" type="email" name="email" required>                          
+                        </div>
+                        <div class="col-md-12">
+                           <textarea class="contactus" placeholder="Mensaje" name="textarea" type="textarea" Message="Name">Mensaje </textarea>
+                        </div>
+                        <div class="col-sm-12">
+                           <button class="send_btn">Enviar</button>
+                        </div>
+                     </div>
+                  </form>
+        <?php
+            #cogemos datos del formulario y los ponemos en variables
+            $nombre_completo=$_POST["full_name"];
+            $tel=$_POST["tel"];
+            $email=$_POST["email"];
+            $texto=$_POST["textarea"];
+            echo $nombre_completo;
+            echo $tel;
+            echo $email;
+            echo $texto;
+
+            #ponemos la consulta en una variable
+            $consulta = "INSERT INTO informacion_personal (nombre_completo,telefono,email,mensaje)VALUES('$nombre_completo','$tel','$email','$texto')";
+            #ponemos la información de conexión en otra (ip/nombreDNS,usuario,contraseña,nombre base de datos)
+            $con=new mysqli('10.0.174.86:3306','root','','clusters');
+            # hacemos la consulta con los datos de la conexión
+            mysqli_query($con , $consulta);
+            # cerramos conexión
+            mysqli_close($con);
+        ?>
+    </body>
+</html>
